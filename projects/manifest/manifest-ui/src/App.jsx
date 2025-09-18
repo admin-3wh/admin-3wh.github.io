6de482ff75340f5cdb5f1cf69d8c1619c56c5b80
admin-3wh <admin@3wh.dev>
2025-09-17 23:19:08 -0500

    Create App.jsx

---
+++ projects/manifest/manifest-ui/src/App.jsx
@@ -0,0 +1,11 @@
+import SearchPanel from './components/SearchPanel';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-white text-gray-900">
+      <SearchPanel />
+    </div>
+  );
+}
+
+export default App;
