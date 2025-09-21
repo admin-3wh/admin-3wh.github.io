533db9f8d9b8a05170f79d3b78a51218ccde9ceb
admin-3wh <admin@3wh.dev>
2025-09-21 17:17:09 -0500

    Update vite.config.js

--- projects/manifest/manifest-ui/vite.config.js
+++ projects/manifest/manifest-ui/vite.config.js
@@ -1,10 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  root: '.',  // or use './projects/manifest/manifest-ui' if outside
   server: {
-    port: 5173,
-  },
+    host: '0.0.0.0', // to expose to local network if needed
+    port: 5173
+  }
 })
